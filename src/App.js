191cc169b7c307e7b8c899cea6d0a546520f6abc
Olaniyi Ajayi <ajayiolaniyi8@gmail.com>
2020-10-07 21:17:47 +0100

    adding of style and comment

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Recipe from './Recipe';
 import './App.css';
 
 
-
+// This is the main app which hold all the component.
 const  App = ()=> {
 
+  // placing the id and api key in an .env file the retrieving it with procees.env
   const APP_ID = process.env.REACT_APP_ID;
 
   const APP_KEY = process.env.REACT_APP_KEY;
 
   // const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+  // Setting recipes array  
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState(''); 
   const [query, setQuery] = useState('chicken')
@@ -22,19 +24,24 @@ const  App = ()=> {
 
   },[ query ]);
 
+  // Using async and await to pull the data from api 
   const getRecipe = async ()=>{
 
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     
+    // setting the recipe array from api
     setRecipes(data.hits);
   }
 
+
+  // get every key stroke from the input 
   const updateSearch = e =>{
     setSearch(e.target.value)
   }
  
 
+  // get the input value fron the input.
   const getSearch = e =>{
 
     e.preventDefault();
@@ -45,7 +52,7 @@ const  App = ()=> {
 
   return (
     <div className="App">
-        <h3>Food App</h3>
+        <h1 className="heading">Food App</h1>
         <form className="search-form" onSubmit={ getSearch }>
           <input className="search-bar" 
           type="text" 
--- src/Recipe.js
+++ src/Recipe.js
@@ -2,13 +2,22 @@ import React from 'react';
 
 const Recipe = ({ title, calories, image,  ingredients })=>{
 
+    // style for recipe component
+    let recipeStyle = {
+        maxWidth:"20rem",
+        padding: "1rem 1rem",
+        margin: "1rem 1rem",
+        border: "1px solid green",
+        borderRadius: "0.5rem"
+    }
+
 
     return(
 
-        <div>
-            <h1>{title}</h1>
+        <div style={recipeStyle}>
+            <h3 style={{textAlign:"center"}}>{title}</h3>
             <ol>{ ingredients.map( ingredient =>(
-                <li>{ingredient.text}</li>
+                <li key={ingredients.indexOf(ingredient)}>{ingredient.text}</li>
             ))}</ol>
             <p>{calories}</p>
             <img src={image} alt=""/>
